ba25a03898abc9cc81c49efd029b9810c562e966
John Pham <johnphammail@gmail.com>
2017-11-11 17:59:06 -0800

    Deploying website

--- src/components/PromoImagesAmit.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="row header-parent">
-        <div class="col-md-3 header-img-container"><img class="header-img" src="../../static/img/amitrc_cover.jpg" alt="The Team"></div>
-        <div class="col-md-3 header-img-container"><img class="header-img" src="../../static/img/image3.png" alt=""></div>
-        <div class="col-md-3 header-img-container"><img class="header-img" src="../../static/img/image4.png" alt=""></div>
-        <div class="col-md-3 header-img-container"><img class="header-img" src="../../static/img/image5.png" alt=""></div>
-      </div>
-</template>
